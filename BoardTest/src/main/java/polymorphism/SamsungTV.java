501a3eff566fefff352e0d3c0f9477a3a33c49d4
안병호 <72555119+ukl0114@users.noreply.github.com>
2024-07-15 11:56:40 +0900

    3일째

--- BoardTest/src/main/java/polymorphism/SamsungTV.java
+++ BoardTest/src/main/java/polymorphism/SamsungTV.java
@@ -1,6 +1,19 @@
 package polymorphism;
 
+
 public class SamsungTV implements TV {
+	
+	public SamsungTV() {
+		System.out.println("====> SamsungTV 객체 생성");
+	}
+	public void initMethod() {
+		System.out.println("객체 초기화 작업 처리...");
+	}
+	
+	public void destroyMethod() {
+		System.out.println("객체 삭제전 처리할 로직...");
+	}
+	
 	public void powerOn() {
 		System.out.println("SamsungTV---전원 켠다.");
 	}
@@ -16,4 +29,5 @@ public class SamsungTV implements TV {
 	public void volumeDown() {
 		System.out.println("SamsungTV---소리 내린다.");
 	}
+	
 }
