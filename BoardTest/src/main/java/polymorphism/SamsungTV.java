811590276d0122f90d5efd3a7ea68317fd4d9ca9
안병호 <72555119+ukl0114@users.noreply.github.com>
2024-07-16 10:35:58 +0900

    4일째

--- BoardTest/src/main/java/polymorphism/SamsungTV.java
+++ BoardTest/src/main/java/polymorphism/SamsungTV.java
@@ -1,11 +1,23 @@
 package polymorphism;
 
-
 public class SamsungTV implements TV {
-	
+	private Speaker speaker;
+	private int price;	
+
 	public SamsungTV() {
-		System.out.println("====> SamsungTV 객체 생성");
+		System.out.println("====> SamsungTV 객체(1) 생성");
 	}
+
+	public void setSpeaker(Speaker speaker) {
+		System.out.println("====> setSpeaker() 호출");
+		this.speaker = speaker;
+	}
+
+	public void setPrice(int price) {
+		System.out.println("====> setPrice() 호출");
+		this.price = price;
+	}
+
 	public void initMethod() {
 		System.out.println("객체 초기화 작업 처리...");
 	}
@@ -15,19 +27,31 @@ public class SamsungTV implements TV {
 	}
 	
 	public void powerOn() {
-		System.out.println("SamsungTV---전원 켠다.");
+		System.out.println("SamsungTV---전원 켠다. (가격 : " + price + ")");
 	}
 	
+	public Speaker getSpeaker() {
+		return speaker;
+	}
+
+	public int getPrice() {
+		return price;
+	}
+
 	public void powerOff() {
 		System.out.println("SamsungTV---전원 끈다.");
 	}
 	
 	public void volumeUp() {
-		System.out.println("SamsungTV---소리 올린다.");
+		/* speaker = new SonySpeaker(); */
+		speaker.volumeUp();
+//		System.out.println("SamsungTV---소리 올린다.");
 	}
 	
 	public void volumeDown() {
-		System.out.println("SamsungTV---소리 내린다.");
+		/* speaker = new SonySpeaker(); */
+		/* speaker = new AppleSpeaker(); */
+		speaker.volumeDown();
+//		System.out.println("SamsungTV---소리 내린다.");
 	}
-	
 }
